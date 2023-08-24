b16e2e68271c88a53d7a21e2f63fe8c9da4a02bb
victor-onoja <odohvictor47@gmail.com>
2023-08-24 11:01:56 +0100

    paused working on this for now

---
+++ js/app.js
@@ -0,0 +1,10 @@
+const userID = document.getElementById('user-id');
+
+var _userID;
+
+// supposed to be gotten from the backend
+_userID = "0xC963…9d01";
+
+userID.innerText = _userID;
+
+// console.log(userID.innerText);
