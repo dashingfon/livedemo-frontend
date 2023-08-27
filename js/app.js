dfd4bc61bb9ca4795c86616cc8a7b1aa6b9b82d0
victor-onoja <odohvictor47@gmail.com>
2023-08-27 01:12:32 +0100

    landing page done... kind of

--- js/app.js
+++ js/app.js
@@ -9,15 +9,52 @@
 
 // console.log(userID.innerText);
 
+
+// read more read less ui
  const textElement = document.getElementById('text');
     const seeMoreButton = document.getElementById('read-more');
 
     seeMoreButton.addEventListener('click', () => {
       if (textElement.style.height === '4.3rem') {
-        textElement.style.height= 'auto'; // Display full text
+        textElement.style.height= 'auto';
         seeMoreButton.textContent = 'See Less';
       } else {
-        textElement.style.height = '4.3rem'; // Display limited text
+        textElement.style.height = '4.3rem'; 
         seeMoreButton.textContent = '...Read More';
-      } // Hide the "See More" button
+      } 
+    });
+
+// toggle element ui
+   const meetOurTeam = document.getElementById('meet-our-team');
+   const ourTeam = document.getElementById('our-team');
+   const teamPics = document.getElementById('team-pics');
+    
+    // const teamContent = document.getElementById('team-content');
+
+    let isExpanded = true;
+
+    meetOurTeam.addEventListener('click', () => {
+      if (isExpanded) {
+        meetOurTeam.style.display = 'none';
+        ourTeam.style.display = 'block';
+        teamPics.style.display = 'flex';
+      } else {
+         meetOurTeam.style.display = 'block';
+        ourTeam.style.display = 'none';
+        teamPics.style.display = 'none';
+      }
+      isExpanded = !isExpanded;
+    });
+
+    ourTeam.addEventListener('click', () => {
+      if (isExpanded) {
+        meetOurTeam.style.display = 'none';
+        ourTeam.style.display = 'block';
+        teamPics.style.display = 'flex';
+      } else {
+         meetOurTeam.style.display = 'block';
+        ourTeam.style.display = 'none';
+        teamPics.style.display = 'none';
+      }
+      isExpanded = !isExpanded;
     });
